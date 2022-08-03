70a7778ecc6bc1e8075efbfdc4993c47b4a96c21
KiwoongSon <kidol9108@smartscore.kr>
2022-08-03 13:17:31 +0900

    Swagger2 기본설정 추가, SampleController 삭제

--- src/main/java/com/kidol/controller/BoardController.java
+++ src/main/java/com/kidol/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.kidol.controller;
 import com.kidol.dto.BoardRequest;
 import com.kidol.dto.BoardResponse;
 import com.kidol.service.BoardService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -16,16 +17,19 @@ public class BoardController {
 
     public final BoardService boardService;
 
+    @ApiOperation(value = "게시글 전체조회")
     @GetMapping("/board")
     public List<BoardResponse> readBoard(){
         return boardService.findAll();
     }
 
+    @ApiOperation(value = "게시글 단건조회 (by boardId)")
     @GetMapping("/board/{boardId}")
     public BoardResponse readBoardDetail(@PathVariable("boardId") Long boardId) throws Exception {
         return boardService.find(boardId);
     }
 
+    @ApiOperation(value = "게시글 등록")
     @PostMapping("/board")
     public Long createBoard(@RequestBody BoardRequest boardRequest){
         return boardService.save(boardRequest);
